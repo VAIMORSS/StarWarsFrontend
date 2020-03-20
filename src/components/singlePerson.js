0917c319fa0e6a27b618ac610009aad4a7d140e8
Vasu Patel <Vasu.Patel@ibm.com>
2020-03-20 14:56:06 -0300

    fix(singlePerson):Changed all arrow function back

--- src/components/singlePerson.js
+++ src/components/singlePerson.js
@@ -1,68 +1,67 @@
-import React, { Component } from 'react'
-import axios from 'axios';
-import InfoModal from './InfoModal';
-
-
+import React, { Component } from "react";
+import axios from "axios";
+import InfoModal from "./InfoModal";
 
 class SinglePerson extends Component {
-
-
     constructor(props) {
         super(props);
         this.state = {
             person: Object,
             buttonPresses: false,
-            modalShow:false,
-            homeworld:Object,
-            species:Object,
-            films:[Object],
-            loading:false
-        }
+            modalShow: false,
+            homeworld: Object,
+            species: Object,
+            films: [Object],
+            loading: false
+        };
+
+
     }
 
-    handleClick = () => {
-
-        this.setState({loading:true})
-        axios.get(`/${this.props.Person.id}`).then((res) => {
-            console.log(res.data.films[0].title)
+    handleClick() {
+        this.setState({ loading: true });
+        axios.get(`/${this.props.Person.id}`).then(res => {
             this.setState({
                 person: res.data,
                 buttonPresses: true,
-                modalShow:true,
-                homeworld:res.data.homeworld,
-                species:res.data.species,
-                films:res.data.films,
-                loading:false
-                
-            })
-        })
-    }
+                modalShow: true,
+                homeworld: res.data.homeworld,
+                species: res.data.species,
+                films: res.data.films,
+                loading: false
+            });
+        });
+    };
 
-    isEmpty = () => {
+    isEmpty() {
         for (var key in this.state.person) {
-            if (this.state.person.hasOwnProperty(key))
-                return false;
+            if (this.state.person.hasOwnProperty(key)) return false;
         }
         return true;
-    }
+    };
+
 
     render() {
         let modalClose = () => this.setState({ modalShow: false });
         return (
             <div className="card">
-                <img className="card-img-top avatar" src="https://starwarsblog.starwars.com/wp-content/uploads/2017/01/sw-the-last-jedi-tall-1200x630.jpg" alt="Card image cap"></img>
+                <img
+                    className="card-img-top avatar"
+                    src="https://starwarsblog.starwars.com/wp-content/uploads/2017/01/sw-the-last-jedi-tall-1200x630.jpg"
+                    alt="Card image cap"
+                ></img>
                 <div className="card-body">
-                    <h5 className="card-title">{this.props.Person.name}</h5> 
+                    <h5 className="card-title">{this.props.Person.name}</h5>
                 </div>
                 <div className="card-footer">
-                    {this.state.loading?
+                    {this.state.loading ? (
                         <div className="lds-hourglass"></div>
-                        :
-                        <button className="btn btn-dark" onClick={this.handleClick}>More Info</button>
-                    }
-                    
-                </div>  
-
+                    ) : (
+                            <button className="btn btn-dark" onClick={this.handleClick}>
+                                More Info
+            </button>
+                        )}
+                </div>
 
                 <InfoModal
                     show={this.state.modalShow}
@@ -71,11 +70,10 @@ class SinglePerson extends Component {
                     homeworld={this.state.homeworld}
                     species={this.state.species}
                     films={this.state.films}
-                    />      
+                />
             </div>
-        )
+        );
     }
 }
-        
-export default SinglePerson;
 
+export default SinglePerson;
